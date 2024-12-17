2a06ed08cdfb194f11c162d76e8ab90c0ed41d87
WEEN <80139938+erwindobp98@users.noreply.github.com>
2024-12-17 20:05:48 +0700

    Update taiko4.py

--- taiko4.py
+++ taiko4.py
@@ -222,7 +222,7 @@ def get_transaction_data_from_taiko(my_address):
     try:
         now = int(time.time())
         start_of_today = int(datetime.datetime.now(datetime.timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0).timestamp())
-        start_of_period = int(datetime.datetime(2024, 9, 16, tzinfo=datetime.timezone.utc).timestamp())
+        start_of_period = int(datetime.datetime(2024, 12, 16, 12, tzinfo=datetime.timezone.utc).timestamp())
         
         url = f'https://api.taikoscan.io/api?module=account&action=txlist&address={my_address}'
         response = requests.get(url)
@@ -312,7 +312,7 @@ def get_final_data(my_address):
 
 def get_rank_data(my_address):
     try:
-        rank_url = f'https://trailblazer.mainnet.taiko.xyz/s2/user/rank?address={my_address}'
+        rank_url = f'https://trailblazer.mainnet.taiko.xyz/s3/user/rank?address={my_address}'
         headers = {
             'User-Agent': 'Mozilla/5.0 (compatible; TaikoRankChecker/1.0)'
         }
@@ -333,7 +333,7 @@ def display_taiko_data(my_address):
 
     if transaction_data:
         animated_print("=================================================================", color=Fore.MAGENTA, delay=0.01)
-        animated_print("                       Taiko Transaction Data S2             ", color=Fore.GREEN, delay=0.01)
+        animated_print("                       Taiko Transaction Data S3             ", color=Fore.GREEN, delay=0.01)
         animated_print("=================================================================", color=Fore.MAGENTA, delay=0.01)
         if 'adjusted_gas_fee_eth_today' in transaction_data:
             animated_print(f"Gas Fee ETH (Hari Ini)       : {transaction_data['adjusted_gas_fee_eth_today']:.10f} ETH", color=Fore.YELLOW, delay=0.01)
@@ -363,7 +363,7 @@ def display_taiko_data(my_address):
         breakdown = rank_data.get('breakdown', 'N/A')
 
         animated_print("=================================================================", color=Fore.MAGENTA, delay=0.01)
-        animated_print("                       Taiko Rank Data S2           ", color=Fore.GREEN, delay=0.02)
+        animated_print("                       Taiko Rank Data S3           ", color=Fore.GREEN, delay=0.02)
         animated_print("=================================================================", color=Fore.MAGENTA, delay=0.01)
         animated_print(f"Rank                         : {rank}", color=Fore.YELLOW, delay=0.01)
         animated_print(f"Score                        : {score}", color=Fore.YELLOW, delay=0.01)
